aec0e2d32e6b8d05f0529fc7cde0b7b9cb702533
Paulo Arthur Rocha <paulo.arthu@hotmail.com>
2020-11-10 12:50:41 -0300

    bad solution

--- src/model/entities/Reservation.java
+++ src/model/entities/Reservation.java
@@ -5,12 +5,11 @@ import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
 public class Reservation {
-
 	private Integer roomNumber;
 	private Date checkIn;
 	private Date checkOut;
 
-	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyy");
+	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
 	public Reservation(Integer roomNumber, Date checkIn, Date checkOut) {
 		this.roomNumber = roomNumber;
@@ -18,11 +17,11 @@ public class Reservation {
 		this.checkOut = checkOut;
 	}
 
-	public Integer getRommNumber() {
+	public Integer getRoomNumber() {
 		return roomNumber;
 	}
 
-	public void setRommNumber(Integer roomNumber) {
+	public void setRoomNumber(Integer roomNumber) {
 		this.roomNumber = roomNumber;
 	}
 
@@ -39,9 +38,19 @@ public class Reservation {
 		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
 	}
 
-	public void updateDates(Date checkIn, Date checkOut) {
+	public String updateDates(Date checkIn, Date checkOut) {
+		Date now = new Date();
+		if (checkIn.before(now) || checkOut.before(now)) {
+			return "Reservation dates for update must be future dates";
+
+		}
+		if (!checkOut.after(checkIn)) {
+			return "Check-out date must be after check-in date";
+		}
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
+		return null;
+
 	}
 
 	@Override
